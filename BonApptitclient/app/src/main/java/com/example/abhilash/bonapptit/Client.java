53e1aea280229798f74b5f64deb0534f79e4eb5f
abhilashsharma3 <42682078+abhilashsharma3@users.noreply.github.com>
2018-12-10 23:21:49 -0800

    commit(abhi)

--- BonApptitclient/app/src/main/java/com/example/abhilash/bonapptit/Client.java
+++ BonApptitclient/app/src/main/java/com/example/abhilash/bonapptit/Client.java
@@ -2,14 +2,34 @@ package com.example.abhilash.bonapptit;
 
 import android.os.AsyncTask;
 
+import java.io.EOFException;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+
 public class Client extends AsyncTask<Void,Void,Void> {
     String destAddress;
     int port;
+    String order;
 
-
-
+    Client(String destAddress,int port,String order){
+        this.destAddress=destAddress;
+        this.order=order;
+        this.port=port;
+    }
     @Override
     protected Void doInBackground(Void... voids) {
-        return null;
+        Socket socket=null;
+        try{
+            socket=new Socket(destAddress,port);
+            
+        } catch (UnknownHostException e){
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
     }
 }
