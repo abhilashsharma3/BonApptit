3b80f97e35d08efe8de0601b3c48495b85495a12
abhilashsharma3 <42682078+abhilashsharma3@users.noreply.github.com>
2018-12-11 16:17:38 -0800

    update client
    Client

--- BonApptitclient/app/src/main/java/com/example/abhilash/bonapptit/Client.java
+++ BonApptitclient/app/src/main/java/com/example/abhilash/bonapptit/Client.java
@@ -1,35 +1,41 @@
 package com.example.abhilash.bonapptit;
 
 import android.os.AsyncTask;
-
-import java.io.EOFException;
 import java.io.IOException;
-import java.lang.reflect.Array;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.ArrayList;
 
-public class Client extends AsyncTask<Void,Void,Void> {
+public class Client extends AsyncTask<String,Void,Void>{
     String destAddress;
     int port;
     String order;
 
-    Client(String destAddress,int port,String order){
-        this.destAddress=destAddress;
-        this.order=order;
-        this.port=port;
-    }
-    @Override
-    protected Void doInBackground(Void... voids) {
-        Socket socket=null;
-        try{
-            socket=new Socket(destAddress,port);
-            
-        } catch (UnknownHostException e){
-            e.printStackTrace();
-        } catch (IOException e){
-            e.printStackTrace();
+        Client(){
+            this.destAddress="192.168.1.15";
+            this.port=456;
         }
 
-    }
-}
+        Client(String order){
+            this.order=order;
+        }
+
+        @Override
+            protected Void doInBackground(String... strings) {
+                Socket socket=null;
+                try{
+                    socket=new Socket(destAddress,port);
+                    OutputStream outputStream=socket.getOutputStream();
+                    PrintStream printStream=new PrintStream(outputStream);
+                    printStream.print(order);
+                    socket.close();
+
+                } catch (UnknownHostException e){
+                    e.printStackTrace();
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        }
