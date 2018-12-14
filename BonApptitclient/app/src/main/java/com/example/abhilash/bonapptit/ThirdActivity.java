4a5e964dedc9e22278f8fa07752483dd92cf34eb
abhilashsharma3 <42682078+abhilashsharma3@users.noreply.github.com>
2018-12-14 11:26:44 -0800

    Update ThirdActivity.java

--- BonApptitclient/app/src/main/java/com/example/abhilash/bonapptit/ThirdActivity.java
+++ BonApptitclient/app/src/main/java/com/example/abhilash/bonapptit/ThirdActivity.java
@@ -95,7 +95,7 @@ public class ThirdActivity extends AppCompatActivity implements View.OnClickList
             if(menuItems.get(menuItems.size() - 1).getQuantity() == 0) {
                 jsonString = jsonString.substring(0, jsonString.length() - 4) + "]";
             }
-            jsonString += ", \"Price\": \"" + price + "\"}";
+            jsonString += ", \"Table\": \"" + bundle.getString("tableNo") + "\", \"Price\": \"" + price + "\"}";
             Log.d("Json String", "jsonString: " + jsonString);
             Intent newIntent = new Intent(ThirdActivity.this, FourthActivity.class);
             bundle.putString("jsonString",jsonString);
@@ -114,27 +114,27 @@ public class ThirdActivity extends AppCompatActivity implements View.OnClickList
                         5.99f,
                         Keys.MENU_BREAKFAST,
                         "Buttermilk pancakes studded with juicy fresh blueberries and topped with a gorgeous deep purple blueberry syrup.",
-                        "blueberry_buttermilk_pancakes.png"));
+                        "blueberry_buttermilk_pancakes"));
                 menuItems.add(new MenuItem("Bagel breakfast Sliders",
                         3.99f,
                         Keys.MENU_BREAKFAST,
                         "Egg whites, tomatoes and spinach in between two bagel halves.",
-                        "bagel_breakfast_sliders.png"));
+                        "bagel_breakfast_sliders"));
                 menuItems.add(new MenuItem("Foodname: Breakfast Strata",
                         7.99f,
                         Keys.MENU_BREAKFAST,
                         "A classic food dish, generally considered a brunch dish, that is traditionally made with layers of ingredients placed over a base of day old bread and fresh cheese, covered with a blended mixture of eggs and milk, and baked in an oven.",
-                        "breakfast_strata.png"));
+                        "breakfast_strata"));
                 menuItems.add(new MenuItem("Classic Coffee Cake",
                         4.99f,
                         Keys.MENU_BREAKFAST,
                         "Coffee cake is a traditional sweet pound cake layered with a brown sugar, cinnamon, and pecan-laced streusel. The reason this beauty is called coffee cake is because it's the perfect complement to a cup of coffee or a foamy latte.",
-                        "classic_coffee_cake.png"));
+                        "classic_coffee_cake"));
                 menuItems.add(new MenuItem("Biscuits 'N Gravy Pie",
                         9.99f,
                         Keys.MENU_BREAKFAST,
                         "Biscuits and gravy is a popular breakfast dish in the United States especially in the South. The dish consists of soft dough biscuits covered in either sawmill or meat gravy made from the drippings of cooked pork sausage white flour milk and often bits of sausage bacon ground beef or other meat",
-                        "biscuits_gravy_pie.png"));
+                        "biscuits_gravy_pie"));
                 break;
             case Keys.MENU_LUNCH:
                 Log.d("Lunch" , " Switch ");
@@ -142,27 +142,27 @@ public class ThirdActivity extends AppCompatActivity implements View.OnClickList
                         5.99f,
                         Keys.MENU_LUNCH,
                         "Chicken served with lettuce, mayonnaise, hard-boiled egg, celery, onion, pepper, pickles (or pickle relish) and a variety of mustards.",
-                        "chicken_salad.png"));
+                        "chicken_salad"));
                 menuItems.add(new MenuItem("Sauteed Shrimp",
                         7.99f,
                         Keys.MENU_LUNCH,
                         "Shrimps sautéed in olive oil, garlic, lemon juice and fresh basil.",
-                        "sauteed_shrimp.png"));
+                        "sauteed_shrimp"));
                 menuItems.add(new MenuItem("Buttered Egg Noodles",
                         4.99f,
                         Keys.MENU_LUNCH,
                         "Buttered Noodles cooked in butter, egg white and chicken broth, topped with  grated Parmesan cheese, parsley, and plenty of butter.",
-                        "buttered_egg_noodles.png"));
+                        "buttered_egg_noodles"));
                 menuItems.add(new MenuItem("Oven-Fried Chicken",
                         10.99f,
                         Keys.MENU_LUNCH,
                         "Simple and lighter fried chicken coated with a seasoned flour mixture and then baked to perfection.",
-                        "oven_fried_chicken.png"));
+                        "oven_fried_chicken"));
                 menuItems.add(new MenuItem("Chicken and Waffles",
                         9.99f,
                         Keys.MENU_LUNCH,
                         "An American Classic. The best known chicken and waffle pairing comes from the American soul food tradition and uses fried chicken.",
-                        "chicken_and_waffles.png"));
+                        "chicken_and_waffles"));
                 break;
             case Keys.MENU_DINNER:
                 Log.d("Dinner" , " Switch ");
@@ -170,27 +170,27 @@ public class ThirdActivity extends AppCompatActivity implements View.OnClickList
                         8.99f,
                         Keys.MENU_DINNER,
                         "Smoked salmon with pasta in a sauce of shallots, garlic, white wine, lemon, and cream, with toasted pine nuts and parsley.",
-                        "smoked_salmon_pasta.png"));
+                        "smoked_salmon_pasta"));
                 menuItems.add(new MenuItem("Cacio e Pepe",
                         9.99f,
                         Keys.MENU_DINNER,
                         "Cacio e Pepe is a pasta dish from Roman cuisine. Cacio e Pepe means cheese and pepper in several central Italian dialects.",
-                        "cacio_e_pepe.png"));
+                        "cacio_e_pepe"));
                 menuItems.add(new MenuItem("Lasagna Roll-Ups",
                         10.99f,
                         Keys.MENU_DINNER,
                         "Lasagna noodle topped with marinara and cooked spinach, then sprinkle with mozzarella and roll up tightly",
-                        "lasagna_roll_ups.png"));
+                        "lasagna_roll_ups"));
                 menuItems.add(new MenuItem("Baked Spaghetti",
                         8.99f,
                         Keys.MENU_DINNER,
                         "A baked pasta dish, made with cheese, meat and vegetables.",
-                        "baked_spaghetti.png"));
+                        "baked_spaghetti"));
                 menuItems.add(new MenuItem("Southwestern Pasta Salad",
                         9.99f,
                         Keys.MENU_DINNER,
                         "A mix of Mexican-inspired ingredients such as black beans, corn, and avocado, along with hearty whole wheat pasta and a light and creamy Greek yogurt paprika dressing, this Southwestern pasta salad boasts simple prep and complex flavor, with wide crowd appeal.",
-                        "southwestern_pasta_salad.png"));
+                        "southwestern_pasta_salad"));
                 break;
         }
     }
